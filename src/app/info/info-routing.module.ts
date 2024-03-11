5f3b4893f4ddda561c295ff9b65a903d318c179e
Joadson <joadsondaniel@gmail.com>
2024-03-11 11:37:33 -0300

    tst

--- src/app/info/info-routing.module.ts
+++ src/app/info/info-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InfoComponent } from './info.component';
+import { ExameComponent } from '../exame/exame.component';
 
 const routes: Routes = [
   { path: '', component: InfoComponent }, 
-  { path: 'exame', loadChildren: () => import('../exame/exame.module').then(m => m.ExameModule) }
+  { path: 'exame', component:ExameComponent, loadChildren: () => import('../exame/exame.module').then(m => m.ExameModule) }
 ];
 
 @NgModule({
