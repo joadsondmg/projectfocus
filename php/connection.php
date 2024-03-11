80b3fdea41b5d06539723ba3b568f88564a7f6d3
Joadson <joadsondaniel@gmail.com>
2024-03-11 13:31:07 -0300

    tst

--- php/connection.php
+++ php/connection.php
@@ -1,15 +1,17 @@
 <?php 
     // Local Connection
-    // $dbHost = 'localhost';
-    // $dbUsername = 'root';
-    // $dbPassword = '';
-    // $dbName = 'focus';
-
-    // Hostinger Connection
     $dbHost = 'localhost';
-    $dbUsername = 'u177475397_rootfocus';
-    $dbPassword = 'Root_control1!@#$%fcs';
-    $dbName = 'u177475397_focus';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'focus';
+
+    // // Hostinger Connection
+    // $dbHost = 'localhost';
+    // $dbUsername = 'u177475397_rootfocus';
+    // $dbPassword = 'Root_control1!@#$%fcs';
+    // $dbName = 'u177475397_focus';
+
+
     $conexao = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
     mysqli_set_charset($conexao, "utf8");
     
