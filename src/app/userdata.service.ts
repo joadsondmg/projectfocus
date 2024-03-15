9aaafafe2dbff528823a527dea14d086462c5079
Joadson <joadsondaniel@gmail.com>
2024-03-15 09:44:19 -0300

    tst

--- src/app/userdata.service.ts
+++ src/app/userdata.service.ts
@@ -12,19 +12,19 @@ export class UserdataService {
   ) { }
 
   // Local Connection
-  // apiGetUser = 'http://localhost/projectfocus/php/user-data.php'
-  // apiGetResult = 'http://localhost/projectfocus/php/valid-exec.php'
-  // apiGetAllResult = 'http://localhost/projectfocus/php/get-results.php'
-  // apiSet = 'http://localhost/projectfocus/php/set-result.php'
-  // apiFilterResult = 'http://localhost/projectfocus/php/filter-result.php'
+  apiGetUser = 'http://localhost/projectfocus/php/user-data.php'
+  apiGetResult = 'http://localhost/projectfocus/php/valid-exec.php'
+  apiGetAllResult = 'http://localhost/projectfocus/php/get-results.php'
+  apiSet = 'http://localhost/projectfocus/php/set-result.php'
+  apiFilterResult = 'http://localhost/projectfocus/php/filter-result.php'
   
 
   // Hosp Connection
-  apiGetUser = 'https://backofficecheck.com.br/focus/php/user-data.php'
-  apiGetResult = 'https://backofficecheck.com.br/focus/php/valid-exec.php'
-  apiGetAllResult = 'https://backofficecheck.com.br/focus/php/get-results.php'
-  apiSet = 'https://backofficecheck.com.br/focus/php/set-result.php'
-  apiFilterResult = 'https://backofficecheck.com.br/focus/php/filter-result.php'
+  // apiGetUser = 'https://backofficecheck.com.br/focus/php/user-data.php'
+  // apiGetResult = 'https://backofficecheck.com.br/focus/php/valid-exec.php'
+  // apiGetAllResult = 'https://backofficecheck.com.br/focus/php/get-results.php'
+  // apiSet = 'https://backofficecheck.com.br/focus/php/set-result.php'
+  // apiFilterResult = 'https://backofficecheck.com.br/focus/php/filter-result.php'
   
   getUserData(token: string): Observable<any> {
     const data = { 
